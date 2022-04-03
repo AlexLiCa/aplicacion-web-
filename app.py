a47a77cd79055b9a213a7141a2a1016c08f83706
AlexLiCa <alexlin.carre@gmail.com>
2022-04-03 14:19:19 -0500

    avances

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 url = "https://apilinares.herokuapp.com/api/tasks"
 
 
- 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
@@ -16,7 +15,7 @@ def home():
             response = requests.get(url).json()['task']
         except:
             response = []
-            print("error") 
+            print("error")
     else:
         name = request.form["name"]
         print(name)
@@ -25,18 +24,23 @@ def home():
             return redirect('/')
         except:
             pass
-    
+
     return render_template('index.html', tasks=response)
 
-@app.route('/update/<int:id>', methods=['GET'])
-def update_task(id):
-    print(f"Vas a actualizar :{id}")
-    return redirect('/')
-    try:
-        pass
-    except:
-        pass
 
+@app.route('/update/<int:id>', methods=['GET', 'PUT'])
+def update_task(id):
+    print(f"\nVas a actualizar {id}\n")
+    # return redirect('/')
+    if request.method == 'GET':
+        try:
+            print(f"{id}")
+            requests.put(url, json ={"check:":True})
+            return redirect('/')
+        except:
+            return redirect('/')
+    else:
+        return redirect('/')
 
 
 
